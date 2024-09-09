03d3c2977862bb4b5492a24bc9e53ec8d4042ec1
OBEONE67 <filmloloto67@gmail.com>
2024-09-09 23:19:43 +0700

    Changes to be committed:

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
 /* GET home page. */
 router.get('/BOOKTYPE1', function(req, res, next) {
   res.render('BOOKTYPE1', { title: 'Express' });
 });
+
+
 
 module.exports = router;
